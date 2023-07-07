900a1a2f1369b6c41ab923f575456911373f736e
Brendan Hannum <90215602+brendan1129@users.noreply.github.com>
2023-07-07 16:13:58 -0400

    Spring.intializr files added
    Using Spring Boot DevTools, Spring Data JPA, Spring Web, PostgreSQL Driver, and Thymeleaf.

---
+++ Project1/src/main/java/com/skillstorm/Project1/WarehouseManagementSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.Project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementSolutionApplication.class, args);
+	}
+
+}
---
+++ Project1/src/test/java/com/skillstorm/Project1/WarehouseManagementSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.Project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
